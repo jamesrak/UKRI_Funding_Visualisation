e3c1d175985253aa988d72dafa0a84048a0c045b
jamesrak <james.rak@outlook.co.th>
2023-05-17 05:10:33 +0100

    add chart and data table in innovate map

--- js/script_innovate_map.js
+++ js/script_innovate_map.js
@@ -25,6 +25,155 @@ var map = new mapboxgl.Map({
     .addTo(map);
 
 
+///////////////////// SIDEBAR ///////////////////////////
+
+/////// Line Chart
+
+// Google Chart Initialisation
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart','table']});
+      google.charts.setOnLoadCallback(drawLineChart);
+	  google.charts.setOnLoadCallback(drawTable);
+
+      function drawLineChart() {
+
+		// grab the CSV
+		$.get("data/IUK-FundedProjects-Funding_by_Sector_pivot.csv", function(csvString) {
+		// transform the CSV string into a 2-dimensional array
+		var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
+		// this new DataTable object holds all the data
+		 // Use object notation to explicitly specify the data type.
+		arrayData[0] = [{type:'string', label:'Year'},
+		{type:'number', label:'AI & Data Economy'},
+		{type:'number', label:'Ageing Society, Health & Nutrition'},
+		{type:'number', label:'Clean Growth & Infrastructure'},
+		{type:'number', label:'Disruptive Technology'},
+		{type:'number', label:'Manufacturing, Materials & Mobility'}]
+		var data = new google.visualization.arrayToDataTable(arrayData);
+		// var data = new google.visualization.DataTable();
+		// data.addColumn('string', 'Year');
+		// data.addColumn('number', 'AI & Data Economy');
+		// data.addColumn('number', 'Ageing Society, Health & Nutrition');
+		// data.addColumn('number', 'Clean Growth & Infrastructure');
+		// data.addColumn('number', 'Disruptive Technology');
+		// data.addColumn('number', 'Manufacturing, Materials & Mobility');
+		// data = new google.visualization.arrayToDataTable(arrayData);
+		// this view can select a subset of the data at a time
+		var view = new google.visualization.DataView(data);
+
+		function formatNumber(num) {
+			if (Math.abs(num) >= 1000000000) {
+			  return (Math.abs(num) / 1000000000).toFixed(1) + 'B';
+			} else if (Math.abs(num) >= 1000000) {
+			  return (Math.abs(num) / 1000000).toFixed(1) + 'M';
+			} else if (Math.abs(num) >= 1000) {
+			  return (Math.abs(num) / 1000).toFixed(1) + 'K';
+			}
+			return num.toString();
+		  }
+
+		for (let i = 1; i <= 5; i++) {
+			var formatter = new google.visualization.NumberFormat({
+				prefix: '£',
+				fractionDigits:0,
+				// suffix:formatNumber(data.getValue(0,i))
+			});
+			formatter.format(data, i);  
+		}
+
+        var options = {
+          title: 'Projects Funded Over Time',
+		  titleTextStyle: {
+			color: '#FFF'
+		},
+		colors: ['rgb(0, 132, 240)', 'rgb(24, 204, 0)', 'rgb(255, 98, 36)','rgb(255, 255, 92)', 'rgb(255, 20, 103)'],
+		//   'width':500,
+		//   'height':300,
+		  chartArea: {width: '65%'},
+          legend: {textStyle: {color: 'white', fontSize: 11}, position: 'top' ,maxLines:3},
+		  'backgroundColor': '#262626',
+		  hAxis: {
+			textStyle:{color: '#FFF'},
+			format: ''
+		},
+		vAxis: {
+			textStyle:{color: '#FFF'},
+			format: 'short'
+		},
+		tooltip: {
+			textStyle:{color: '#000'},
+			format: 'short'
+		},
+		chartArea: {
+            left: 60,
+            right: 70
+          },
+		  focusTarget: 'category'
+        };  
+
+        var chart = new google.visualization.LineChart(document.getElementById('funded_by_year_chart'));
+
+        chart.draw(view, options);
+		});
+      }
+
+	  function drawTable() {
+		// grab the CSV
+		$.get("data/IUK-FundedProjects-2004-2022-Dataset.csv", function(csvString) {
+			// transform the CSV string into a 2-dimensional array
+			var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
+			var data = new google.visualization.arrayToDataTable(arrayData);
+			var numberFormatter = new google.visualization.NumberFormat({
+				prefix: '£',
+				pattern: 'short'
+			  });
+
+			numberFormatter.format(data, 7); // Apply formatting to the 'Amount' column
+
+			var colorFormatter = new google.visualization.ColorFormat();
+			colorFormatter.addRange('Manufacturing, Materials & Mobility', 'Manufacturing, Materials & Mobility', 'rgb(255, 20, 103)', '#FFFFFF'); //#1a1a1a
+			colorFormatter.addRange('Ageing Society, Health & Nutrition', 'Ageing Society, Health & Nutrition', 'red', 'white'); //#1a1a1a
+			colorFormatter.addRange('2018/19','2018/19', 'red', 'white'); //#1a1a1a
+	  
+			colorFormatter.format(data, 1); // Apply formatting to the 'sector' column
+
+
+			var view = new google.visualization.DataView(data);
+			view.setColumns([3,2,4,1,7,12,11]);
+			// Sort the DataView by 'amount' column in descending order
+			view.setRows(data.getSortedRows({
+				column: 7,  // Sort by the 'amount' column (index 1)
+				desc: true  // Sort in descending order
+			  }));
+
+			// Apply conditional formatting on the 'sector' column
+			// view.setRows(data.getFilteredRows([{column: 1, value: 'Manufacturing, Materials & Mobility'}]));
+
+			var table = new google.visualization.Table(document.getElementById('funded_table'));
+
+			options = {
+				page: 'enable',
+				pageSize: 5,
+				showRowNumber: true,
+				allowHtml: true,
+				cssClassNames: {
+					headerRow: 'header-row',
+					tableRow: 'table-row',
+					oddTableRow: 'odd-table-row',
+					selectedTableRow: 'selected-table-row',
+					hoverTableRow: 'hover-table-row',
+					headerCell: 'header-cell',
+					tableCell: 'table-cell',
+					rowNumberCell: 'row-number-cell'
+					},
+			};
+
+			table.draw(view, options);
+		});
+	  }
+
+
+
 map.on('load', function(){
     // map.setLayoutProperty('innovate_uk-2021', 'visibility', 'visible');
     
